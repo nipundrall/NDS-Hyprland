c9862df4889dd39059f0b0f02eb704c2f57b49bf
NIPUN DRALL <161411550+nipundrall@users.noreply.github.com>
2024-05-13 11:01:23 +0530

    Add files via upload

---
+++ waybar/modules/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cowsay "Updating the System"
+
+cowsay " By Pacman First"
+
+sudo pacman -Syu
+
+cowsay " Now by Yay"
+
+yay -Syu
+
+cowsay "I've Updated Everything on your System"
+
+cowsay " Long Live $USER "
