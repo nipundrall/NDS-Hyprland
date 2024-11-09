6fda18bdc1f1303d89e13ed0b8769b9a74581100
Nipun Drall <161411550+nipundrall@users.noreply.github.com>
2024-11-09 08:33:48 +0530

    Update volume.sh

--- hypr/modules/volume.sh
+++ hypr/modules/volume.sh
@@ -5,7 +5,6 @@ statusLine=$(amixer get Master | tail -n 1)
 status=$(echo "${statusLine}" | grep -wo "on")
 volume=$(echo "${statusLine}" | awk -F ' ' '{print $5}' | tr -d '[]%')
 
-#echo "$volume"
 
 # Getting argument {volup,voldown,volmute}
 
